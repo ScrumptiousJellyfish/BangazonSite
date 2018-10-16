ef666f21e81ccdd1afecfbc406ed940e30ab4a81
Jenn Lawson <jenniferlawson000@gmail.com>
2018-10-16 14:12:52 -0500

    added product type view model

---
+++ Bangazon/Models/ProductTypesViewModels/ProductTypesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bangazon.Models.ProductTypesViewModels
+{
+    public class ProductTypesViewModel
+    {
+        public IEnumerable<Product> Product { get; set; }
+
+        public ProductType ProductType { get; set; }
+    }
+}
