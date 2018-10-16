a57e347e7f2df53a61fd64ebf5b61b6b7550bab6
Evan Lusky <ewlusky@gmail.com>
2018-10-16 15:33:31 -0500

    home page items ready for test

--- Bangazon/Controllers/HomeController.cs
+++ Bangazon/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace Bangazon.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var applicationDbContext = _context.Product.OrderBy(p => p.DateCreated).Take(20);
+            var applicationDbContext = _context.Product.OrderByDescending(p => p.DateCreated).Take(20);
             return View(await applicationDbContext.ToListAsync());
             
         }
