93d2773d189d4498413788b51083e7115026f7ee
Evan Lusky <ewlusky@gmail.com>
2018-10-17 10:46:27 -0500

    test home about privacy contact endpoints

--- BangazonTests/UnitTest1.cs
+++ BangazonTests/UnitTest1.cs
@@ -1,14 +1,38 @@
 using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
-namespace BangazonTests
+namespace BangazonTest
 {
-    public class UnitTest1
+    public class ExerciseTest : IClassFixture<WebApplicationFactory<Bangazon.Startup>>
     {
-        [Fact]
-        public void Test1()
-        {
+        private readonly WebApplicationFactory<Bangazon.Startup> _factory;
 
+        public ExerciseTest(WebApplicationFactory<Bangazon.Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Home")]
+        [InlineData("/Home/About")]
+        [InlineData("/Home/Privacy")]
+        [InlineData("/Home/Contact")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
         }
     }
 }
